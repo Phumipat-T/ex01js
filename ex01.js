c83e6e690f4cb53a4bbcae598756539a3c68e3d1
Phumipat Thirawat <phumipat_thirawat@elearning.cmu.ac.th>
2021-05-28 14:03:02 +0700

    Commit1

---
+++ ex01.js
@@ -0,0 +1,26 @@
+//var x
+//x = 12 + 36
+//bmi = 32 * x
+//console.log(bmi)
+// ---
+
+var x = 0
+var y = 0
+while (x < 10) {
+    console.log('ค่า x ตอนนี้คือ ' + x)
+    console.log('ค่า y ตอนนี้คือ ' + y)
+    x = x + 1
+    y = y + 1
+}
+
+//var score = 80
+//var grade
+//if (score > 80) {
+//    grade = 'A'
+//} else if (score > 60) {
+//    grade = 'B'
+//} else {
+//   grade = 'C'
+//}
+
+
